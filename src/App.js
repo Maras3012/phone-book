898ce8ef190998cae415a92135a736e0cb4842ef
Maras <marasovi1@gmail.com>
2021-02-06 22:32:14 +0100

    firebase test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
 import React, { Component } from 'react';
 
+import firebase from 'firebase';
+import { firebaseConfig } from './config';
+
 class App extends Component {
+  constructor() {
+    super()
+
+    this.app = firebase.initializeApp(firebaseConfig);
+    this.database = this.app.database().ref().child('number');
+
+    this.state = {
+      number: 0
+    }
+  }
+
+  componentDidMount() {
+    this.database.on('value', snap => {
+      this.setState({
+        number:  snap.val()
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>HAHAHAHAHAHAHAHA</h1>
+        <h1>Value: {this.state.number}</h1>
       </div>
     )
   }
---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyABZM_O8lGoMQUXXuZDNFVzzuAoyGCUjGk",
+    authDomain: "my-work-one-e5d31.firebaseapp.com",
+    projectId: "my-work-one-e5d31",
+    storageBucket: "my-work-one-e5d31.appspot.com",
+    messagingSenderId: "740891273265",
+    appId: "1:740891273265:web:26e8534abed630b99e0d10",
+    measurementId: "G-EQNVKNJXPD"
+  };
