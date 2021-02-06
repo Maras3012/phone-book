265e0be48b8ce080a2063528623fe284e9a8035a
Maras <marasovi1@gmail.com>
2021-02-06 21:37:41 +0100

    function in class based

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>HAHAHAHAHAHAHAHA</h1>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>HAHAHAHAHAHAHAHA</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
+
+/* function App() {
+  return (
+    <div className="App">
+      <h1>HAHAHAHAHAHAHAHA</h1>
+    </div>
+  );
+} */
--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  // Strict mode checks are run in development mode only; they do not impact the production build.
+  <React.StrictMode> 
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
