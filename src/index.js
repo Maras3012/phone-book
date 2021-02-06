cff63dccf1a57dbbb49c28740f320428ac8d5c7f
Maras <marasovi1@gmail.com>
2021-02-06 21:46:12 +0100

    function in class based after build

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  // Strict mode checks are run in development mode only; they do not impact the production build.
   <React.StrictMode> 
     <App />
   </React.StrictMode>,
