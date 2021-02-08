a53204465f5b114a7807cc5a8b73ef5aa6dd1200
Maras <marasovi1@gmail.com>
2021-02-09 00:44:56 +0100

    header,footer,logo,background 1.4

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Toolbar.css'
 import Logo from '../Logo/Logo';
 
-const toolbar = (props) => (
+class Toolbar extends Component {
+  render() {
+    return (
+// const toolbar = (props) => (
     <header className="Toolbar">
       <div><b>NOTES</b></div>
       <Logo />
       <div><b>SIGN OUT</b></div>
     </header>  
-)
+    )
+  }
+}    
 
-export default toolbar;
+export default Toolbar;
